fb954a25394708fd42fc33c14ef83dd08c270d2e
MaxBenches <113772275+MaxBenches@users.noreply.github.com>
2023-09-17 21:22:32 +0200

    Tweaked code

--- main.py
+++ main.py
@@ -2,17 +2,11 @@ import blink
 import motor
 import UDP
 
-blink.blink_pico(3)
 
 def main():
 
-    motor.M_left(1)
-    motor.M_right(1)
-    motor.M_left(-1)
-    motor.M_right(-1)
-
     # UDP setup has been tested and works
-    """
+
     sock, UDP_IP, UDP_PORT = UDP.UDP_setup("0.0.0.0", 5005, "BenchNet", "happyjungle592")
     sock.bind((UDP_IP, UDP_PORT))
     blink.blink_pico(3)
@@ -20,7 +14,7 @@ def main():
     while True:
         message = UDP.msg_receive(sock, 1024, ("utf-8"))
         print(message)
-    """
+
 
 
 main()
